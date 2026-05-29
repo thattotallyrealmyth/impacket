23943023e86bba5f3cc4718282122419006c8cb3
ThatTotallyRealMyth <106909154+ThatTotallyRealMyth@users.noreply.github.com>
2026-05-29 11:36:20 +1000

    created firewal-lsecurity file

---
+++ impacket/dcerpc/v5/fasp.py
@@ -0,0 +1,17 @@
+# Impacket - Collection of Python classes for working with network protocols.
+#
+# Copyright Fortra, LLC and its affiliated companies 
+#
+# All rights reserved.
+#
+# This software is provided under a slightly modified version
+# of the Apache Software License. See the accompanying LICENSE file
+# for more information.
+#
+# Description:
+#   MS-FASP (Firewall and Advanced Security Protocol) implementation. This protool is an RPC based protocol that revolves around
+#   the management of Windows Firewall rules and policies and working/interfacing with all things Windows Firewall related remotely over
+#   RPC interfaces.
+# Author: Abdul Mhanni
+
+
