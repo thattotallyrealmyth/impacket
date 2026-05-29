78fd911501ffe09aae3ca429f68b928ee0a9f1a1
ThatTotallyRealMyth <106909154+ThatTotallyRealMyth@users.noreply.github.com>
2026-05-29 13:51:26 +1000

    comments

--- impacket/dcerpc/v5/fasp.py
+++ impacket/dcerpc/v5/fasp.py
@@ -12,6 +12,9 @@
 #   MS-FASP (Firewall and Advanced Security Protocol) implementation. This protool is an RPC based protocol that revolves around
 #   the management of Windows Firewall rules and policies and working/interfacing with all things Windows Firewall related remotely over
 #   RPC interfaces.
+#   
+#   TLDR:  the network administrator needs to be ableto remotely manage the advanced firewall and IPsec policy of a host and this is
+#    the protocol that allows them to do so.
 # Author: Abdul Mhanni
 
 
